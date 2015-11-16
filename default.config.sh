5f12493fa48ba2980fe2146872b6657cda26e475
Amitai Burstein <amitai@gizra.com>
2015-11-16 22:42:01 +0200

    Add config for Github

--- default.config.sh
+++ default.config.sh
@@ -68,7 +68,15 @@ MYSQL_DB_NAME="hedley"
 ##
 
 # Post install script.
-# function post_install {}
+function post_install {
+  chmod 777 www/sites/default/settings.php
+
+  # Github integration.
+  echo "\$conf['hedley_github_client_id'] = '<your-client-id>';" >> www/sites/default/settings.php
+  echo "\$conf['hedley_github_client_secret'] = '<your-client-secret>';"  >> www/sites/default/settings.php
+  echo "\$conf['hedley_user_github_dummy'] = '<github-user-access-token>';"  >> www/sites/default/settings.php
+}
+
 
 # Post upgrade script.
 # function post_upgrade {}
